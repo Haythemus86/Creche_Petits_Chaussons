49b92c3d40d45d9fb5cfa2b19326e67a08c55a84
Haythem <i-tem@live.fr>
2019-09-04 17:36:32 +0200

    Ajout de bootstrap sur demandeponctuelle mise en place verification date javascript

--- WebContent/JSP/DemandePonctuelle.jsp
+++ WebContent/JSP/DemandePonctuelle.jsp
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <meta charset="UTF-8">
 <style><%@ include file="../CSS/DemandePonctuelle.css" %></style>
 <title>Demande Ponctuelle</title>
@@ -28,9 +29,9 @@
 		
 		<form method="post">
 		
-			<label for="dateGardeEnfantArrivee">Selectionner la date et l'heure d'arrivée</label><input type="datetime-local" id="dateGardeEnfantArrivee" name="dateGardeEnfantArrivee" required="required">
+			<label for="dateGardeEnfantArrivee">Selectionner la date et l'heure d'arrivée</label><input type="datetime-local" id="dateGardeEnfantArrivee" name="dateGardeEnfantArrivee" min="" required="required">
 			
-			<label for="dateGardeEnfantDepart">Selectionner la date et l'heure de départ</label><input type="datetime-local" id="dateGardeEnfantDepart" name="dateGardeEnfantDepart" required="required"> 
+			<label for="dateGardeEnfantDepart">Selectionner la date et l'heure de départ</label><input type="datetime-local" id="dateGardeEnfantDepart" name="dateGardeEnfantDepart" min="" required="required"> 
 	
 			<label for="email">e-mail</label><input type="email" id="email" name="email" value="adresse mail" required="required">
 			
@@ -48,6 +49,25 @@
 			</script>
 		</c:if>
 		
+		<script type="text/javascript">	
+	 	(function() {
+	 		 		
+	 		
+	 		var dateActuel = new Date();
+
+	 		var datestring = dateActuel.getFullYear() + "-" + ("0"+(dateActuel.getMonth()+1)).slice(-2) + "-" + ("0" + dateActuel.getDate()).slice(-2) + "T"+ ("0" + dateActuel.getHours()).slice(-2) + ":" + ("0" + dateActuel.getMinutes()).slice(-2);
+	 		
+	 	 		
+			var dateArrivee = document.getElementById("dateGardeEnfantArrivee");
+			var dateDepart = document.getElementById("dateGardeEnfantDepart");
+			console.log("date du jour " + datestring );
+			
+			dateArrivee.setAttribute("min" , datestring);
+			dateDepart.setAttribute("min", datestring );
+	 		
+	 	})()
+		</script>
+		
 		
 		  	
 		
@@ -58,5 +78,8 @@
 
 
 	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- src/com/gennevilliers/mail/Email.java
+++ src/com/gennevilliers/mail/Email.java
@@ -15,70 +15,67 @@ import com.gennevilliers.beans.Parents;
 public class Email {
 	private String username = "haythem75008@gmail.com";
 	private String password = "hercules86";
-	
 
-public void envoyer(String email) throws MessagingException, RuntimeException {
+	public void envoyer(String email) throws MessagingException, RuntimeException {
 // Etape 1 : Création de la session
-Properties props = new Properties();
-props.put("mail.smtp.auth", "true");
-props.put("mail.smtp.starttls.enable","true");
-props.put("mail.smtp.host","smtp.gmail.com");
-props.put("mail.smtp.port","587");
-Session session = Session.getInstance(props,
-new javax.mail.Authenticator() {
-protected PasswordAuthentication getPasswordAuthentication() {
-return new PasswordAuthentication(username, password);
-}
-});
-try {
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		try {
 // Etape 2 : Création de l'objet Message
-Message message = new MimeMessage(session);
-message.setFrom(new InternetAddress(username));
-message.setRecipients(Message.RecipientType.TO,
-InternetAddress.parse(email));
-message.setSubject("Test email");
-message.setText("Bonjour, ce message est un test ...");
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+			message.setSubject("Test email");
+			message.setText("Bonjour, ce message est un test ...");
 // Etape 3 : Envoyer le message
-Transport.send(message);
-System.out.println("Message_envoye");
-} catch (MessagingException e) {
-throw new RuntimeException(e);
-} }
+			Transport.send(message);
+			System.out.println("Message_envoye");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-public void envoyerRecapitulatifDemandePonctuelle(DemandePonctuelle demande) throws MessagingException, RuntimeException {
-	
-	// Etape 1 : Création de la session
-	Properties props = new Properties();
-	props.put("mail.smtp.auth", "true");
-	props.put("mail.smtp.starttls.enable","true");
-	props.put("mail.smtp.host","smtp.gmail.com");
-	props.put("mail.smtp.port","587");
-	Session session = Session.getInstance(props,
-	new javax.mail.Authenticator() {
-	protected PasswordAuthentication getPasswordAuthentication() {
-	return new PasswordAuthentication(username, password);
+	public void envoyerRecapitulatifDemandePonctuelle(DemandePonctuelle demande)
+			throws MessagingException, RuntimeException {
+
+		// Etape 1 : Création de la session
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			// Etape 2 : Création de l'objet Message
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(demande.getEmail() + ",i-tem@live.fr"));
+			message.setSubject("Demande de garde Ponctuelle");
+			message.setContent("Bonjour,\n Voici un résumé de votre demande : <br />" 
+			        + "<strong> Date d'arrivée :  "	+ demande.getDateArrivee() +"</strong> <br />" 
+					+ "<strong> Date de départ : " + demande.getDateDepart() + "</strong> <br />"
+					+ "Nous vous enverrons dans les plus brefs délais une réponse à votre adressse mail <strong> " + demande.getEmail() +  "</strong> <br />"
+					+"Merci de votre confiance.", "text/html ; charset=UTF-8");
+			// Etape 3 : Envoyer le message
+			Transport.send(message);
+			System.out.println("Message_envoye");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
-	});
-	
-	
-	try {
-	// Etape 2 : Création de l'objet Message
-	Message message = new MimeMessage(session);
-	message.setFrom(new InternetAddress(username));
-	message.setRecipients(Message.RecipientType.TO,
-	InternetAddress.parse(demande.getEmail()+",i-tem@live.fr"));
-	message.setSubject("Demande de garde Ponctuelle");
-	message.setText("Bonjour,\n Voici un résumé de votre demande : \n"
-			+ "\t Date d'arrivée : " + demande.getDateArrivee()+ "\n"
-					+ "\t Date de départ : " + demande.getDateDepart() + "\n"
-							+ "Nous vous enverrons dans les plus brefs délais une réponse par mail.\n Merci de"
-							+ "votre confiance.");
-	// Etape 3 : Envoyer le message
-	Transport.send(message);
-	System.out.println("Message_envoye");
-	} catch (MessagingException e) {
-	throw new RuntimeException(e);
-	}
-	
-}
 }
